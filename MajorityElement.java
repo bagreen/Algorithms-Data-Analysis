ab01417fec346c9c9685c1d578759abce15aa7b5
Ben Green <benjaminangusgreen@gmail.com>
2018-09-20 10:47:28 -0700

    Update MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,3 +1,6 @@
+// Given: A positive integer k ≤ 20, a positive integer n ≤ 10^4, and k arrays of size n containing positive integers not exceeding 10^5.
+// Return: For each array, output an element of this array occurring strictly more than n / 2 times if such element exists, and "-1" otherwise.
+
 import java.util.Arrays;
 
 public class MajorityElement {
