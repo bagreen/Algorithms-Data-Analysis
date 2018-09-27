274c07991e9e09bed438db819cf20efcc3d13e88
Ben Green <benjaminangusgreen@gmail.com>
2018-09-26 20:17:09 -0700

    Update ConnectedComponents.java

--- ConnectedComponents.java
+++ ConnectedComponents.java
@@ -1,81 +1,48 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class ConnectedComponents {
-    public static void main(String[] args) {
-        // need to add method somewhere that checks to see if a new line
-        // was put in that made a value you had already put in a new row
-        // maybe instead store the data in a arraylist and then use the .contains
-        // method to see if either of two points is in arraylist for each line?
-        // still doesn't solve my ad hoc problem...
+    static class Node {
+        int key;
+        boolean visited;
+        java.util.ArrayList<Node> neighbors;
 
-
-
-
-
-
-        // adding file
-        In in = new In("CCtest.txt");
-        int points = in.readInt(), lines = in.readInt();
-
-        // numbers[] is everything left over in file
-        int[] data = in.readAllInts();
-
-        // neighbors[][] is a list of each point's neighbor
-        // one extra row at bottom to store if value appeared yet
-        int[][] neighbors = new int[points][points];
-
-        int total = 0, emptyColumn = 1;
-
-        // adds degrees and neighbors
-        for (int i = 0; i <= data.length - 1; i += 2) {
-            int first = data[i] - 1;
-            int second = data[i + 1] - 1;
-
-            if (i == 0) {
-                neighbors[0][first] = first + 1; neighbors[0][second] = second + 1;
-                neighbors[points - 1][first] = first + 1; neighbors[points - 1][second] = second + 1;
-                total++;
-            }
-
-            // something's fishy here
-            // not copying over the array like it's supposed to
-            for (int k = 1; k <= points - 2; k++) {
-                for (int l = 0; l <= points - 2; l++) {
-                    if (neighbors[k][l] > 0 && neighbors[k - 1][l] > 0) {
-                        for (int m = 0; m < points - 1; m++) {
-                            neighbors[k - 1][m] = neighbors[k][m];
-                            neighbors[k][m] = 0;
-                        }
-                    }
-                }
-            }
-
-            for (int j = 0; j <= points - 1; j++) {
-                if (neighbors[j][first] == first + 1) {
-                    neighbors[j][second] = second + 1;
-                    neighbors[points - 1][second] = second + 1;
-                    break;
-                }
-                else if (neighbors[j][second] == 1) {
-                    neighbors[j][first] = 1;
-                    neighbors[points - 1][first] = first + 1;
-                    break;
-                }
-                else if (j == points - 1) {
-                    neighbors[emptyColumn][first] = first + 1; neighbors[emptyColumn][second] = second + 1;
-                    neighbors[points - 1][first] = first + 1; neighbors[points - 1][second] = second + 1;
-                    emptyColumn++; total++;
-                }
+        Node(int key) {
+            this.key = key;
+            this.visited = false;
+            this.neighbors = new java.util.ArrayList<>();
+        }
+    }
+    private static void traverse(Node node) {
+        node.visited = true;
+        for (Node i : node.neighbors) {
+            if (!i.visited) {
+                traverse(i);
             }
         }
+    }
+    public static void main(String[] args) throws java.io.IOException {
+        java.io.File file = new java.io.File("Resources/rosalind_cc(2).txt");
+        java.util.Scanner in = new java.util.Scanner(file);
 
-        System.out.println(" 1  2  3  4  5  6  7  8  9  10 11 12");
+        int pointNum = in.nextInt(), lineNum = in.nextInt();
+        Node[] points = new Node[pointNum];
 
-        for (int[] row : neighbors) {
-            System.out.println(Arrays.toString(row));
+        for (int i = 0; i < pointNum; i++) {
+            points[i] = new Node(i);
         }
 
-        System.out.println(total);
+        for (int j = 0; j < lineNum; j++) {
+            int first = in.nextInt() - 1, second = in.nextInt() - 1;
+            points[first].neighbors.add(points[second]);
+            points[second].neighbors.add(points[first]);
+        }
+
+        int shapeNum = 0;
+
+        for (Node k : points) {
+            if (!k.visited) {
+                shapeNum++;
+                traverse(k);
+            }
+        }
+        System.out.println(shapeNum);
     }
 }
